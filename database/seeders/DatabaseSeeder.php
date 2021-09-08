b42b526ff0362cbe9a44fc69d91c200959ba4651
megavad2507 <54946556+megavad2507@users.noreply.github.com>
2021-09-08 20:55:49 +0300

    часть 36 добавление сидов с ску

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(UsersTableSeeder::class);
-        $this->call(CategoriesTableSeeder::class);
-        $this->call(ProductsTableSeeder::class);
         $this->call(CurrencySeeder::class);
+        $this->call(ContentSeeder::class);
     }
 }
